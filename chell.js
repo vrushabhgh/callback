73c7573a222ab2b3da93af07d4fbd18e8b68f374
vrushabh gawande <79848424+vrushabhgh@users.noreply.github.com>
2021-04-20 14:28:14 +0530

    Add files via upload

---
+++ chell.js
@@ -0,0 +1,16 @@
+
+      function timedText() {
+        var x = document.getElementById("txt");
+        setTimeout(function(){ x.innerHTML="<h1>10<h1> " }, 1000);
+        setTimeout(function(){ x.innerHTML="<h1>9 <h1>" }, 2000);
+        setTimeout(function(){ x.innerHTML="<h1>8<h1> " }, 3000);
+        setTimeout(function(){ x.innerHTML="<h1>7<h1> " }, 4000);
+        setTimeout(function(){ x.innerHTML="<h1>6 <h1>" }, 5000);
+        setTimeout(function(){ x.innerHTML="<h1>5<h1> " }, 6000);
+        setTimeout(function(){ x.innerHTML="<h1>4<h1> " }, 7000);
+        setTimeout(function(){ x.innerHTML="<h1>3<h1> " }, 8000);
+        setTimeout(function(){ x.innerHTML="<h1>2<h1> " }, 9000);
+        setTimeout(function(){ x.innerHTML="<h1>1<h1>" }, 10000);
+        setTimeout(function(){ x.innerHTML="<h1>happy birthdy<h1> " }, 11000);
+      }
+      
